93c6af70276a70126876770c13629da18099353c
Bolorunduro Winner-Timothy <ogatimo@gmail.com>
2022-02-24 18:57:50 +0100

    ft: change icon and sizing

--- whatsapp-web-duo/App.axaml.cs
+++ whatsapp-web-duo/App.axaml.cs
@@ -2,23 +2,22 @@ using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 
-namespace whatsapp_web_duo
+namespace whatsapp_web_duo;
+
+public partial class App : Application
 {
-    public partial class App : Application
+    public override void Initialize()
     {
-        public override void Initialize()
+        AvaloniaXamlLoader.Load(this);
+    }
+
+    public override void OnFrameworkInitializationCompleted()
+    {
+        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
-            AvaloniaXamlLoader.Load(this);
+            desktop.MainWindow = new MainWindow();
         }
 
-        public override void OnFrameworkInitializationCompleted()
-        {
-            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
-            {
-                desktop.MainWindow = new MainWindow();
-            }
-
-            base.OnFrameworkInitializationCompleted();
-        }
+        base.OnFrameworkInitializationCompleted();
     }
 }
--- whatsapp-web-duo/MainWindow.axaml.cs
+++ whatsapp-web-duo/MainWindow.axaml.cs
@@ -1,20 +1,8 @@
 using Avalonia.Controls;
-using WebViewControl;
 
-namespace whatsapp_web_duo
+namespace whatsapp_web_duo;
+
+public partial class MainWindow : Window
 {
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-
-        private void TabSelectionChanged(object? sender, SelectionChangedEventArgs e)
-        {
-            var selectedIndex = (sender as TabControl)?.SelectedIndex;
-
-            var webview = new WebView();
-        }
-    }
+    public MainWindow() => InitializeComponent();
 }
